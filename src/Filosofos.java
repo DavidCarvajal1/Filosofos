6652b6a5b0ae044859bc8d8e2370385a1931588a
DavidCarvajal1 <david.carvajal@iesnervion.es>
2022-12-03 14:18:59 +0100

    Terminado

--- src/Filosofos.java
+++ src/Filosofos.java
@@ -1,57 +1,43 @@
 public class Filosofos implements Runnable {
     int id;
-    static boolean[] filosofos=new boolean[5];
-    public Filosofos(int id){
-        this.id=id;
+    static boolean[] tenedores = new boolean[5];
+
+    public Filosofos(int id) {
+        this.id = id;
     }
+
     @Override
     public void run() {
+        int tenedorIzquierda = id;
+        int tenedorDerecha = id + 1;
+        if (tenedorDerecha == 5) {
+            tenedorDerecha = 0;
+        }
         while (true) {
             try {
-                int tenedorIzquierda=id;
-                int tenedorDerecha=id+1;
-                if(tenedorDerecha==5){
-                    tenedorDerecha=0;
-                }
-                long tiempoDeEspera;
-                boolean tiempoDeEsperaSuperado=false;
-                //Intentamos coger el primer tenedor
-                synchronized (filosofos) {
-                    while (filosofos[tenedorIzquierda]) {
-                        filosofos.wait();
-                    }
-                    filosofos[tenedorIzquierda] = true;
-                }
-                System.out.println("El filosofo " + id + " cogio el tenedor izquierdo "+tenedorIzquierda);
-                //Intentamos coger el segundo tenedor
-                //Si espera mas de X tiempo, soltara el primer tenedor, sino cogera el derecho
-                synchronized (filosofos) {
-                    tiempoDeEspera=System.currentTimeMillis();
-                    while (filosofos[tenedorDerecha]&&!tiempoDeEsperaSuperado) {
-                        filosofos.wait();
-                        if(System.currentTimeMillis()-tiempoDeEspera>10000){
-                            tiempoDeEsperaSuperado=true;
-                        }
-                    }
-                    if(!filosofos[tenedorDerecha]) {
-                        filosofos[tenedorDerecha] = true;
-                        System.out.println("El filosofo " + id + " cogio el tenedor derecho "+(tenedorDerecha));
-                        Thread.sleep((int) (Math.random() * 3000) + 2000);
-                        //Soltamos ambos libros
-                        synchronized (filosofos) {
-                            filosofos[tenedorIzquierda] = false;
-                            filosofos[tenedorDerecha] = false;
-                            filosofos.notifyAll();
-                        }
-                        System.out.println("El filosofo " + id + " soloto los tenedores "+tenedorIzquierda+" "+(tenedorDerecha));
 
-                    }else{
-                        filosofos[tenedorIzquierda]=false;
-                        System.out.println("El filosofo "+id+" solto los tenedor por tiempo de espera superado");
+                synchronized (tenedores){
+                    //Esperamos a que los tenedores esten libres
+                    while (tenedores[tenedorDerecha]||tenedores[tenedorIzquierda]){
+                        tenedores.wait();
                     }
+                    //El filosofo coge los tenedores
+                    tenedores[tenedorDerecha]=true;
+                    System.out.println("El filosofo "+id+" cogio el tenedor derecho "+tenedorDerecha);
+                    tenedores[tenedorIzquierda]=true;
+                    System.out.println("El filosofo "+id+" cogio el tenedor izquierdo "+tenedorIzquierda);
                 }
-
-
+                //El filosofo come
+                Thread.sleep((int)(Math.random()*4000));
+                synchronized (tenedores){
+                    //El filosofo suelta los dos tenedores
+                    tenedores[tenedorDerecha]=false;
+                    tenedores[tenedorIzquierda]=false;
+                    tenedores.notifyAll();
+                    System.out.println("El filosofo "+id+" solto los tenedores "+tenedorDerecha+" "+tenedorIzquierda);
+                }
+                //El filosofo piensa
+                Thread.sleep((int)(Math.random()*4000));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -59,14 +45,10 @@ public class Filosofos implements Runnable {
     }
 
 
-
-
     public static void main(String[] args) {
         //Lanzamos a los 5 filosofos
         for (int i = 0; i < 5; i++) {
             new Thread(new Filosofos(i)).start();
         }
-        //Lanzamos la clase que notificara a los filosofos cada 10 sec
-        new Thread(new ActualzarFilosofo()).start();
     }
 }
